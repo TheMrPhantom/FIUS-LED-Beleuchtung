713427037fd7efe00cbe53fb4fb35e4746b1e350
haslersn <sebastian.hasler@gmx.net>
2018-08-04 17:48:09 +0200

    In the template, changed the default serial port to /dev/ttyS3

--- makeConfig.template.mk
+++ makeConfig.template.mk
@@ -1,4 +1,4 @@
 # If this file is called "makeConfig.mk", it contains your local configuration and is ignored by git.
 
 # The serial port where the ESP32 board is or will be connected
-UPLOAD_PORT = /dev/ttyS1
+UPLOAD_PORT = /dev/ttyS3
