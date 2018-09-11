b62045628a34ccefaf54c146ff853e407f601f37
TheMrPhantom <TheMrPhantom@users.noreply.github.com>
2018-09-11 16:56:40 +0200

    White fix 2

--- src/WhiteState.cpp
+++ src/WhiteState.cpp
@@ -2,7 +2,7 @@
 #include "LedStrip.hpp"
 
 WhiteState::WhiteState(LedStrip &led_strip) {
-    led_strip.SetColors(CRGB{150, 150, 150});
+    led_strip.SetColors(CRGB{100, 100, 100});
 }
 
 void WhiteState::Update() {}
