3b38bc23319876824e2ddc41d3948ddb10fcef3f
TheMrPhantom <TheMrPhantom@users.noreply.github.com>
2018-09-11 16:48:52 +0200

    Brighter white

--- src/WhiteState.cpp
+++ src/WhiteState.cpp
@@ -2,7 +2,7 @@
 #include "LedStrip.hpp"
 
 WhiteState::WhiteState(LedStrip &led_strip) {
-    led_strip.SetColors(CRGB{10, 10, 10});
+    led_strip.SetColors(CRGB{200, 200, 200});
 }
 
 void WhiteState::Update() {}
