490b4bf9b951f249eec397e57242c932ee3dcd46
TheMrPhantom <TheMrPhantom@users.noreply.github.com>
2018-12-03 16:30:05 +0100

    fix7

--- src/states/Christmas.hpp
+++ src/states/Christmas.hpp
@@ -14,7 +14,7 @@ class Christmas : public State {
 
     const CRGB red = CRGB(120, 0, 0);
     const CRGB green = CRGB(40, 255, 0);
-    const CRGB spark = CRGB(120, 120, 0);
+    const CRGB spark = CRGB(255, 255, 0);
     const int sparkSize=5;
     const int lightSize=10; 
     LedStrip &strip;
